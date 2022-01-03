256f7a9ee54c18c26b3e9750c048fe8036427a2e
Dmitry <dp_140482@mail.ru>
2022-01-03 12:11:19 +0300

    ДЗ 6

--- lesson6/server.js
+++ lesson6/server.js
@@ -25,13 +25,36 @@ const server = http.createServer((request, response) => {
 });
 
 const socket = io(server);
+let counter = 0;
 
 socket.on("connection", socket => {
-    console.log('New connection');
-    socket.broadcast.emit('NEW_CONN_EVENT', { msg: 'The new client connected' });
+    console.log("New connection: " + socket.id);
+    counter++;
+    socket.broadcast.emit("NEW_CONN_EVENT", {
+        msg: "Client " + socket.id + " connected",
+        count: counter
+    });
+    socket.emit("NEW_CONN_EVENT", {
+        msg: "I am connected",
+        count: counter
+    });
 
-    socket.on('CLIENT_MSG', data => {
-        socket.emit('SERVER_MSG', { msg: data.msg.split('').reverse().join('') });
+    socket.on("CLIENT_MSG", data => {
+        socket.emit("SERVER_MSG", { msg: "I wrote: " + data.msg });
+        socket.broadcast.emit("SERVER_MSG", { msg: socket.id + ": " + data.msg });
+    });
+
+    socket.on("disconnect", reason => {
+        console.log("Connection " + socket.id + " finished: " + reason);
+        counter--;
+        socket.broadcast.emit("DISCONN_EVENT", {
+            msg: "Client " + socket.id + " disconnected: " + reason,
+            count: counter
+        });
+        socket.emit("DISCONN_EVENT", {
+            msg: "I am disconnected: " + reason,
+            count: counter
+        });
     });
 });
 
