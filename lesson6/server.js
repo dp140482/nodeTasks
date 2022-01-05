3b759f48536eaf83ceea34f605bb8c5064fb719a
Dmitry <dp_140482@mail.ru>
2022-01-05 12:42:33 +0300

    Добавление имён пользователей

--- lesson6/server.js
+++ lesson6/server.js
@@ -25,36 +25,67 @@ const server = http.createServer((request, response) => {
 });
 
 const socket = io(server);
-let counter = 0;
+let activeUsers = [];
+let users = [];
+let usersID = [];
 
 socket.on("connection", socket => {
     console.log("New connection: " + socket.id);
-    counter++;
-    socket.broadcast.emit("NEW_CONN_EVENT", {
-        msg: "Client " + socket.id + " connected",
-        count: counter
-    });
-    socket.emit("NEW_CONN_EVENT", {
-        msg: "I am connected",
-        count: counter
+
+    socket.on("NEW_USER", userName => {
+        if (!users.includes(userName)) {
+            users.push(userName);
+            activeUsers.push(userName);
+            usersID.push(socket.id);
+            console.log("New client: " + userName + " (" + socket.id + ")");
+            socket.broadcast.emit("NEW_CONN_EVENT", {
+                msg: "Client " + userName + " connected",
+                users: activeUsers
+            });
+            socket.emit("NEW_CONN_EVENT", {
+                msg: "I am connected",
+                users: activeUsers
+            });
+        } else {
+            console.log("Client " + userName + " returns with id " + socket.id);
+            activeUsers.push(userName);
+            const index = users.indexOf(userName);
+            if (socket.id !== usersID[index]) {
+                socket.emit("RECONN_EVENT", {
+                    msg: "Client " + userName + " reconnected",
+                    users: activeUsers
+                });
+                socket.broadcast.emit("RECONN_EVENT", {
+                    msg: "Client " + userName + " reconnected",
+                    users: activeUsers
+                });
+                usersID[index] = socket.id;
+                console.log("Client " + userName + " reconnected");
+            }
+        }
     });
 
     socket.on("CLIENT_MSG", data => {
         socket.emit("SERVER_MSG", { msg: "I wrote: " + data.msg });
-        socket.broadcast.emit("SERVER_MSG", { msg: socket.id + ": " + data.msg });
+        const userName = users[usersID.indexOf(socket.id)];
+        socket.broadcast.emit("SERVER_MSG", { msg: userName + ": " + data.msg });
     });
 
     socket.on("disconnect", reason => {
         console.log("Connection " + socket.id + " finished: " + reason);
-        counter--;
-        socket.broadcast.emit("DISCONN_EVENT", {
-            msg: "Client " + socket.id + " disconnected: " + reason,
-            count: counter
-        });
-        socket.emit("DISCONN_EVENT", {
-            msg: "I am disconnected: " + reason,
-            count: counter
-        });
+        const userName = users[usersID.indexOf(socket.id)];
+        const index = activeUsers.indexOf(userName);
+        activeUsers.splice(index, 1);
+        if (userName) {
+            socket.broadcast.emit("DISCONN_EVENT", {
+                msg: "Client " + userName + " disconnected: " + reason,
+                users: activeUsers
+            });
+            socket.emit("DISCONN_EVENT", {
+                msg: "I am disconnected: " + reason,
+                users: activeUsers
+            });
+        }
     });
 });
 
